785569451f1959cd6c41721232c3f08bb93a3fc6
maisaayousef <130254505+maisaayousef@users.noreply.github.com>
2023-11-07 13:32:26 +0300

    upadte

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-
-import 'bootstrap/dist/css/bootstrap.css'
-import './App.css'
-import axios from 'axios'
-import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [data,setData]=useState({})
-  let response=axios
-  .get('https://api.themoviedb.org/3/movie/550?api_key=2ee808060e23b727aeb4492951e11394')
-  .then(response=>(console.log(`you could ${response.data}`)))
-  .catch(err=>{
-    console.log(`ERROR! ${err}`)
-  })
+  const [data, setData] = useState([]);
+  const [search,setserch]=useState('')
   
-
-  const mystyle={
-    width:'14.75rem',
+  useEffect(() => {
+    // Fetch movie data when the component mounts
+    axios
+      .get('https://api.themoviedb.org/3/movie/popular?api_key=d7835d06aa9bf95f73ab15509e7dc769&query=${search}')
+      .then(response => {
+        setData(response.data.results);
+      })
+      .catch(err => {
+        console.log(`ERROR! ${err}`);
+      });
+  }, [search]); 
+  function searchmovie() {
+    setserch("")
     
-   
-    
+ 
 
   }
- 
+  const mystyle = {
+    width: '14.75rem',}
+  
+
 
   return (
     <>
@@ -31,48 +37,26 @@ function App() {
       <a href='#'> Trending</a>
       </div>
       <div >
-        <input type='text' placeholder='Movie search'/>
-        <button className='buttonheader' >search</button>
+        <input type='text' placeholder='Movie search' value={search}/>
+        <button className='buttonheader' onClick={searchmovie()} >search</button>
       </div>
     </div>
+    
     <div className='sec'>
-    <div className='container d-flex justify-content-around flex-grow flex-wrap '>
-    <div className="card bg-secondary " style={mystyle}  >
-      <img src='ret.jpg' className="card-img-top" alt="..."/>
-      <h5 className="card-title text-light text-center">Retribution</h5>
-      <a href="#" className="btn btn-dark text-light ">View more</a>
-    </div>
-    <div className="card bg-secondary" style={mystyle}  >
-      <img src='muzzle.jpg' className="card-img-top" alt="..."/>
-      <h5 className="card-title text-light text-center">Muzzle</h5>
-      <a href="#" className="btn btn-dark text-light ">View more</a>
-    </div>
-    <div className="card bg-secondary " style={mystyle}  >
-      <img src='five.jpg' className="card-img-top" alt="..."/>
-      <h5 className="card-title text-light text-center">Five Nights at Freddy's</h5>
-      <a href="#" className="btn btn-dark text-light ">View more</a>
-    </div>
-    <div className="card bg-secondary " style={mystyle}  >
-      <img src='fast.jpg' className="card-img-top" alt="..."/>
-      <h5 className="card-title text-light text-center">Fast X</h5>
-      <a href="#" className="btn btn-dark text-light ">View more</a>
-    </div>
-    <div className="card bg-secondary" style={mystyle}  >
-      <img src='exor.jpg' className="card-img-top" alt="..."/>
-      <h5 className="card-title text-light text-center">The Exorcist: Believer</h5>
-      <a href="#" className="btn btn-dark text-light ">View more</a>
-    </div>
-    <div className="card bg-secondary " style={mystyle}  >
-      <img src='coco.jpg' className="card-img-top" alt="..."/>
-      <h5 className="card-title text-light text-center">Coco</h5>
-      <a href="#" className="btn btn-dark text-light ">View more</a>
-    </div>
+        <div className='container d-flex justify-content-around flex-grow flex-wrap '>
+          {data.map(movie => (
+            <div className="card bg-secondary" style={mystyle} key={movie.id}>
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+              <h5 className="card-title text-light text-center">{movie.title}</h5>
+              <a href="#" className="btn btn-dark text-light">View more</a>
+            </div>
+          ))}
    </div>
    </div>
 
       
     </>
-  )
-}
+  
+        )  }
 
 export default App
